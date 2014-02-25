a5d1e3b65826073359b2fdd9484f9dce85ba1bdd
Juan Ricardo V.Gutierrez <pikitgb@gmail.com>
2014-02-25 01:36:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fastscrum::Application.config.secret_key_base = '140c946fcb79e81300a563ca571c067acb5f46baa265d3858a56117be3bcb75799e8d6edd8558a946526fac48e40f58a570a669c52eae303a45578ad01393928'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fastscrum::Application.config.session_store :cookie_store, key: '_fastscrum_session'
